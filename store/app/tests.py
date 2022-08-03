56ce15b56257f658d69836be24b760b5e8df355a
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-03 21:20:13 +0200

    Saving

--- store/app/tests.py
+++ store/app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib import admin
+from .models import *
+# Register your models here.
+class InstructorAdmin(admin.ModelAdmin):
+    fields = ['user','full_time']
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+    
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'name', 'description']
+    inlines = [LessonInline]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Instructor, InstructorAdmin)
