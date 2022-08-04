e1737f190a8ee410c057a6746f6e89f261dc514a
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-04 18:53:33 +0200

    editing admin folder

--- store/app/admin.py
+++ store/app/admin.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-class InstructorAdmin(admin.ModelAdmin):
-    fields = ['first_name','full_time']
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
-    
-class CourseAdmin(admin.ModelAdmin):
-    fields = ['name', 'description']
-    inlines = [LessonInline]
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Instructor, InstructorAdmin)
+admin.site.register(Course)
+admin.site.register(Instructor)
