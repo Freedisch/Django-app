c8c0ba16309f91b62502db7f5b10eaa2e8f48610
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-04 23:32:58 +0200

    Creating enroll view

--- store/app/views.py
+++ store/app/views.py
@@ -1,9 +1,10 @@
-import imp
-from re import template
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import date
-from app.models import *
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
 # Create your views here.
 def popular_course_list(request):
     context = {}
@@ -15,3 +16,15 @@ def popular_course_list(request):
         # Appen the course list as an entry of context dict
         context['course_list'] = course_list
         return render(request, 'onlinecourse/course_list.html', context)
+
+def enroll(request, course_id):
+    # If request method is POST
+    if request.method == 'POST':
+        #First try to read the course object 
+        # If could be found, raise a 404 exception 
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        # Return a HTTP response redirecting user to course list view
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
